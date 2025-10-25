a57fd174d322e755b4abe7b2236028fb3a350d2e
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2025-10-25 23:19:14 +0200

    Add Code for H2 (Minima und Maxima)

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,36 @@
 package h2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.random.RandomGenerator;
+
 public class H2_main {
+
+    public static void main(String[] args) {
+
+        int i = RandomGenerator.getDefault().nextInt(-20, 20);
+        int j = RandomGenerator.getDefault().nextInt(-20, 20);
+        int k = RandomGenerator.getDefault().nextInt(-20, 20);
+        int min;
+        int max;
+
+        List<Integer> list = new ArrayList<>();
+        list.add(i);
+        list.add(j);
+        list.add(k);
+
+        System.out.println(list.toString());
+
+        list.sort(Integer::compareTo);
+
+        System.out.println(list.toString());
+
+        min = list.getFirst();
+        max = list.getLast();
+
+        System.out.println(min);
+        System.out.println(max);
+
+    }
+
 }

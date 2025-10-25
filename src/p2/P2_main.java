cacc552f446457eca14ab698e0bc9310184b4d47
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2025-10-25 18:53:25 +0200

    Add Code for P2

--- src/p2/P2_main.java
+++ src/p2/P2_main.java
@@ -1,4 +1,21 @@
 package p2;
 
+import java.util.random.RandomGenerator;
+
 public class P2_main {
+
+    public static void main(String[] args) {
+
+        int i = RandomGenerator.getDefault().nextInt(-10, 10);
+        boolean b = RandomGenerator.getDefault().nextBoolean();
+
+        System.out.println(i + " " + b);
+
+        if (i > 10) {
+            b = true;
+        }
+
+        System.out.println(i + " " + b);
+    }
+
 }

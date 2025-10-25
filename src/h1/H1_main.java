0c3136404e9ca43f1c3a0124c43c219f647189a6
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2025-10-25 23:18:55 +0200

    Add Code for H1 (Bedingte Subtraktion)

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,4 +1,19 @@
 package h1;
 
+import java.util.random.RandomGenerator;
+
 public class H1_main {
+
+    public static void main(String[] args) {
+
+        int z = RandomGenerator.getDefault().nextInt(0, 20);
+
+        System.out.println(z);
+
+        if (z > 10) z -= 10;
+
+        System.out.println(z);
+
+    }
+
 }

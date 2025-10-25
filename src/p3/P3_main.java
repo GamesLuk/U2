a3a5b2a12b48c0801d258b4591669a4d5022a6c8
GamesLuk <lukas@meyer-zu-berstenhorst.de>
2025-10-25 19:09:04 +0200

    Add Code for P3

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -1,4 +1,33 @@
 package p3;
 
+import java.util.random.RandomGenerator;
+
 public class P3_main {
+
+    public static void main(String[] args) {
+
+        int i = RandomGenerator.getDefault().nextInt(-10, 10);
+        int j = RandomGenerator.getDefault().nextInt(-10, 10);
+        int k = RandomGenerator.getDefault().nextInt(-10, 10);
+
+        System.out.println(i + " " + j + " " + k);
+
+        if (i < j) System.out.println("A");
+        if (i < k) System.out.println("B");
+        if (j < k) System.out.println("C");
+
+    }
 }
+
+/* Aufgaben zum Code:
+
+    Möglichkeiten:
+        - nichts    0 0 0
+        - A         1 2 1
+        - C         3 1 2
+        - AB        1 2 2
+        - BC        0 0 1
+        - ABC       1 2 3
+
+
+ */
